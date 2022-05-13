67c0cb98a647a58546b726edec896151da329dfc
Heikkilä Elias <elias.heikkila@aalto.fi>
2022-05-13 22:18:28 +0300

    hhhmmm

--- julia_howard/src/julia_howard.jl
+++ julia_howard/src/julia_howard.jl
@@ -3,48 +3,50 @@ module julia_howard
 using DSP
 using Random
 using LinearAlgebra
+using StaticArrays
 using Hadamard
 using Kronecker
 
-mm = 4
-N = 2^mm
-IdN = Diagonal(ones(N))
+const mm = 3
+const N = 2^mm
+const IdN = Diagonal(ones(N))
 
-smat = rand((0,1), mm, mm)
-smat = (smat + transpose(smat)) .% 2 + Diagonal(rand((0,1), mm))
-bvec = rand((0,1), mm)
-
-display(smat)
-println("")
-display(bvec)
-println("")
 
 function generate_chirp(smat, bvec)
-    chirp = zeros(ComplexF64, N)
+    xvec = Vector{Int64}(undef,mm)
+    multvec1 = Vector{Float64}(undef, mm)
+    multint1 = Float64
+    multint2 = Float64
+    chirp = Vector{ComplexF64}(undef,N)
     for i in 1:N
-        xvec = reverse(digits(i-1, base=2, pad=mm))
-        chirp[i] = im^(xvec'*smat*xvec + 2*bvec'*xvec)
+        digits!(xvec,i-1, base=2)
+        mul!(multvec1,smat,xvec)
+        multint1 = bvec' * xvec
+        multint2 = multvec1' * xvec
+        chirp[i] = im^(multint1 + 2*multint2)
+        # chirp[i] = im^(xvec * smat * xvec + 2*bvec'*xvec)
     end
     return chirp
 end
 
-chirp = generate_chirp(smat, bvec)
 
 # Decoder
 
-Z = [1 0; 0 -1]
-X = [0 1; 1 0]
-I = [1 0; 0 1]
+const Z = [1 0; 0 -1]
+const X = [0 1; 1 0]
+const I = [1 0; 0 1]
 seq = [X]
 for i in 1:(mm-1); push!(seq, I); end
 
-global perms = []
+global permss = []
 for i in 1:mm
-    push!(perms, kron(seq...))
+    push!(permss, kron(seq...))
     global seq = circshift(seq, 1)
 end
 
-function decoderow(vecin, row)
+global const perms = permss
+
+function decoderow(vecin::Vector{ComplexF64}, row::Int64)
     shiftmultiplied = conj(vecin) .* (perms[row]*vecin)
     wht = fwht_natural(shiftmultiplied)
     avec = zeros(mm); avec[row] = 1
@@ -76,14 +78,37 @@ function project(vecin, eigenvalue, a, aS)
     return projector*vecin
 end
 
-sest = zeros(Int8, mm, mm)
-best = zeros(Int8, mm)
-for row in 1:mm
-    sest[row, :], best[row] = decoderow(chirp, row)
+function decode(vecin)
+    sest = zeros(Int8, mm, mm)
+    best = zeros(Int8, mm)
+    for row in 1:mm
+
+        sest[row, :], best[row] = decoderow(vecin, row)
+    end
+    return sest, best
 end
 
-display(sest)
-display(best)
-display(construct_pauli([0,0,0],[1,1,1]))
+function simulate()
+    drops = 1
+    errors = 0
+    # SNR in dB scale
+    SNR = 10
+    smat = Matrix{Int8}
+    bvec = Vector{Int8}
+    for i in 1:drops
+        smat = rand((0,1), mm, mm)
+        smat = (smat + transpose(smat)) .% 2 + Diagonal(rand((0,1), mm))
+        bvec = rand((0,1), mm)  
+        tstvec = generate_chirp(smat, bvec)
+        # Add noise
+        sest, best = decode(tstvec)
+        if (smat != sest) | (bvec != best)
+            errors += 1
+        end
+    end
+    #println(errors)
+end
+
+simulate()
 
 end # module
